575b07c0d2e5e90afdf8cd43bda6aba71e95ffd0
hapiz2022 <112926419+hapiz2022@users.noreply.github.com>
2022-11-11 08:57:25 +0700

    Praktikum-2

---
+++ C-Task2/Menghitung Total.cpp
@@ -0,0 +1,14 @@
+//…programmenghitunghargatotal
+#include <iostream>
+using namespace std;
+#define harga 4500.02 //…menentukan harga dengan definevoid main()
+
+int main(){  
+    float jumlah,total;
+    
+    cout << "Masukan jumlah barang = ";
+    cin  >> jumlah; total=harga*jumlah;
+
+    cout << "\nYang harus di bayar = " << total;
+    return 0;
+}
